41a6c2904810585499d24b14a55f10cb8631d542
AlexisCanasZ <alexis91296@gmail.com>
2020-09-13 16:38:27 -0500

    Added the /docs folder to deploy it

---
+++ docs/precache-manifest.bfc5bef4ea00acbc890ccbabd970296c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24e30c00923392c3b811a96e2b95f2e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11a4ee64ee764ce43e91",
+    "url": "/static/css/main.075ee5e2.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11a4ee64ee764ce43e91",
+    "url": "/static/js/main.4e32fafe.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
